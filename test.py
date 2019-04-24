0c69f0cd3cb006ccc0b33792a351268ff4629cfb
Nima Seyedtalebi <nseyedtalebi@gmail.com>
2019-04-23 20:02:41 -0400

    Added more stuff to the test code.

--- test.py
+++ test.py
@@ -12,9 +12,14 @@ for project in conn.listProjects():
 		for image in dataset.listChildren():
 			if image.getROICount() > 0:
 				print "Found ROIs for image %s" % image.getName()
+				#list this one on the index page
 				roi_service = conn.getRoiService()
 				result = roi_service.findByImage(image.getId(), None)
 				for roi in result.rois:
 					for s in roi.copyShapes():
-						#prints way too much info
+						#prints way too much info, would be good for ragged shapes
 						#print roi_service.getPoints(s.getId().getValue())
+						#print s
+						#For now, just get rectangle working
+						if type(s) == omero.model.RectangleI:
+							print("Hey!")
