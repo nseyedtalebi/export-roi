663b008acae75518859e4ac37f9be81bd92a5174
Nima Seyedtalebi <nseyedtalebi@gmail.com>
2019-04-24 13:17:08 -0400

    Starting to add stuff to get roi patches

--- omero_exportml/exportml/urls.py
+++ omero_exportml/exportml/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'get_roi_patches/(\d+)', views.get_roi_patches,name='get_roi_patches')
 ]
--- omero_exportml/exportml/views.py
+++ omero_exportml/exportml/views.py
@@ -32,5 +32,22 @@ def index(request):
 			p['datasets'].append(ds)
 		if has_images:
 			projects.append(p)
-
+	conn.close()
 	return render(request, 'exportml/index.html',{'projects':projects,'thumbnails':thumbnails})
+
+def get_roi_patches(request,image_id):
+	conn = BlitzGateway('root','omero-root-password',host='192.168.1.22')
+	if not conn.connect():
+		return HttpResponseServerError("Could not connect to Omero server")
+	roi_service = conn.getRoiService()
+	result = roi_service.findByImage(long(image_id), None)
+	#for roi in result.rois:
+	#	for s in roi.copyShapes():
+	#		#prints way too much info, would be good for ragged shapes
+	#		#print roi_service.getPoints(s.getId().getValue())
+	#		#print s
+	#		#For now, just get rectangle working
+	#		if type(s) == omero.model.RectangleI:
+	#			print("Hey!")'''
+	rois = [result.rois]
+	return HttpResponse(rois)
